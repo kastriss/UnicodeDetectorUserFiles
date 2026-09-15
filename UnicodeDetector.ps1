36a19207d21271b0e91c5078e627e4b2d1dde31b
kastris_ <kastriskastris@gmail.com>
2026-09-15 15:49:44 +0300

    UnicodeDetector.ps1

--- UnicodeDetector.ps1
+++ UnicodeDetector.ps1
@@ -53,7 +53,7 @@ foreach ($Path in $TargetPaths) {
 
 # Credits (Cause I am the best)
 Write-Host "[*] Scan complete." -ForegroundColor Cyan
-Write-Host "Made with love by kastris_`n" -ForegroundColor Magenta
+Write-Host "Made by kastris_`n" -ForegroundColor Magenta
 
 if ($FoundCount -eq 0) {
     Write-Host "[+] Clean! No .exe or .dll file paths contain Unicode characters." -ForegroundColor Green
